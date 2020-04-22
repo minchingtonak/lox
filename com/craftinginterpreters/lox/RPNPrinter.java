5b5928b3cb1955ae97bdabc2e01e216b5e644029
Alec Minchington <alec.minch@gmail.com>
2020-04-22 12:54:51 -0400

    Add RPN style ast printer

---
+++ com/craftinginterpreters/lox/RPNPrinter.java
@@ -0,0 +1,56 @@
+package com.craftinginterpreters.lox;
+
+import com.craftinginterpreters.lox.Expr.Binary;
+import com.craftinginterpreters.lox.Expr.Grouping;
+import com.craftinginterpreters.lox.Expr.Literal;
+import com.craftinginterpreters.lox.Expr.Unary;
+import com.craftinginterpreters.lox.Expr.Visitor;
+
+class RPNPrinter implements Visitor<String> {
+
+    public String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Binary expr) {
+        return RPNFormat(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Grouping expr) {
+        return expr.expression.accept(this); // Don't print anything for a grouping, just recurse
+    }
+
+    @Override
+    public String visitLiteralExpr(Literal expr) {
+        return expr.value == null ? "nil" : expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Unary expr) {
+        return RPNFormat(expr.operator.lexeme, expr.right);
+    }
+
+    private String RPNFormat(String lexeme, Expr... exprs) {
+        StringBuilder sb = new StringBuilder();
+
+        for (Expr expr : exprs) {
+            sb.append(expr.accept(this));
+            sb.append(" ");
+        }
+
+        return sb.append(lexeme).toString();
+    }
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Grouping(new Expr.Binary(new Expr.Literal(1),
+                        new Token(TokenType.PLUS, "+", null, 1), new Expr.Literal(2))),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(new Expr.Binary(new Expr.Literal(4),
+                        new Token(TokenType.MINUS, "-", null, 1), new Expr.Literal(3))));
+
+        System.out.println(new RPNPrinter().print(expression));
+    }
+}
